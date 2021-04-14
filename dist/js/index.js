809f3c9999e0a187b45b880825e4e5ca68865ca1
Yaroslav Pikiner <yaroyay@gmail.com>
2021-04-14 19:21:24 +0300

    fix name and password rules

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,62 +3,97 @@ const ValidateForm = (function () {
   const inputPassword = document.getElementById('password-input');
   const inputCountry = document.getElementById('country-select');
   const inputName = document.getElementById('full-name');
+
   const errName = document.querySelector('.form__error--name');
   const errPass = document.querySelector('.form__error--password');
   const errCountry = document.querySelector('.form__error--country');
+
   const termCheckbox = document.getElementById('terms');
   let formBtn = document.querySelector('.form__btn');
-  formBtn.setAttribute('disabled','true');
+  formBtn.setAttribute('disabled', 'true');
 
   return {
-    tooglePassword: function () {
-      if (inputPassword.type === 'password') {
-        inputPassword.type = 'text';
-      } else {
-        inputPassword.type = 'password';
-      }
-    },
-    validate: function () {
-      const passRegExp = /^(?=.*\d)(?=.*[a-z])[0-9a-z!@#$%^&*]{4,}$/;
-      const nameRegExp = /^[0-9a-zA-Z  ]{1,}$/;
 
-      const passResult = passRegExp.test(inputPassword.value);
+    validate: function (e) {
+      e.preventDefault();
+      console.log(this.validateName(),  this.validatePassword(), this.validateCountry());
+      if(this.validateName() && this.validatePassword && this.validateCountry()) {
+        console.log('Logged')
+      } else {
+        console.log('Not autorized')
+      }
+      return true;
+    },
+  
+
+ 
+
+    validateName: function () {
+      const nameRegExp = /^[0-9a-zA-Z^\s ]{1,}$/;
       const nameResult = nameRegExp.test(inputName.value);
 
       if (!nameResult) {
         inputName.classList.add('form__error');
         errName.style.display = 'block';
-        errName.innerHTML = `
-            <p> *Name is required fields </p>  
-            <p> *Min 3 words </p> 
-            <p> *Name must contain only letters </p>`;
+        errName.innerHTML = `<p> *Name is required fields </p>`;
         return false;
       }
+      if(inputName.value.trim() === '') {
+        inputPassword.classList.add('form__error');
+        errName.style.display = 'block';
+        errName.innerHTML = `<p>*Only space not allowed</p>`;
+        return false;
+      }
+      errName.style.display = 'none';
+      inputName.classList.remove('form__error');
+      return true;
+    },
 
+    validatePassword: function () {
+      const passRegExp = /^[0-9a-zA-Z]{4,}$/;
+      const passResult = passRegExp.test(inputPassword.value);
       if (!passResult) {
         inputPassword.classList.add('form__error');
         errPass.style.display = 'block';
-        errPass.innerHTML = `
-          <p> *Password is required fields </p>  
-          <p> *Contain at least 1 uppercase character </p> 
-          <p> *Contain at least 8 characters </p> 
-          <p> *Contains only 0-9a-zA-Z <p/>`;
+        errPass.innerHTML = `<p>*Password is required fields</p>`;
+        return false;
+      } 
+
+      if(inputPassword.value.length <= 3) {
+        inputPassword.classList.add('form__error');
+        errPass.style.display = 'block';
+        errPass.innerHTML = `<p>*Min 4 letters</p>`;
         return false;
       }
+      errPass.style.display = 'none';
+      inputPassword.classList.remove('form__error');
+      return true;
+    },
 
+    validateCountry: function() {
       if (!inputCountry.value) {
         inputCountry.classList.add('form__error');
         errCountry.style.display = 'block';
         errPass.textContent = '*Chose your country';
         return false;
       }
-
-      document.querySelector('.form__error--password').style.display = 'none';
+      errCountry.style.display = 'none';
+      inputCountry.classList.remove('form__error');
       return true;
     },
 
-    btnCheck: function() {
-      termCheckbox.checked ? formBtn.removeAttribute('disabled') : formBtn.setAttribute('disabled', 'true')
-    }
+    btnCheck: function () {
+      termCheckbox.checked
+        ? formBtn.removeAttribute('disabled')
+        : formBtn.setAttribute('disabled', 'true');
+    },
+
+      tooglePassword: function () {
+      if (inputPassword.type === 'password') {
+        inputPassword.type = 'text';
+      } else {
+        inputPassword.type = 'password';
+      }
+    },
   };
 })();
